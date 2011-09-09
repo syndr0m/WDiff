75a04937c211982527c349f972a336b747039d4d
Marc Dassonneville <marc.dassonneville@gmail.com>
2011-09-09 16:06:02 +0200

    Implementing max process limit on phantomjs. (default = 5)

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express')
    , spawn = require('child_process').spawn
    , everyone = nowjs.initialize(server)
 
+var phantomProcess = 5
+
 /**
  * @param url string
  * @param options { page : { settings : { viewportSize : { ... }, userAgent : { ... } }, 
@@ -11,12 +13,22 @@ var express = require('express')
  */
 everyone.now.screenshot = function (url, options, callback) {
    var client = this.now
-      , screenshot = "static/screenshots/"+new Date().getTime()+".png"
+      , screenshot = "static/screenshots/"+new Date().getTime()+".png";
    
-   console.log('spawning ./phantomjs rasterize.js '+url+' '+screenshot+' '+JSON.stringify(options))
-   var phantom = spawn('./phantomjs', ["rasterize.js", url, screenshot, JSON.stringify(options) ])
-   phantom.stdout.on('data', function (o) { console.log(o.toString('utf8')) })
-   phantom.on('exit', function (e) { callback(e?"/static/error.png":"/"+screenshot) })
+   (function () {
+      if (phantomProcess == 0) {
+         console.log('waiting...')
+         return setTimeout(arguments.callee, 1000)
+      }
+      --phantomProcess
+      console.log('spawning ./phantomjs rasterize.js '+url+' '+screenshot+' '+JSON.stringify(options))
+      var phantom = spawn('./phantomjs', ["rasterize.js", url, screenshot, JSON.stringify(options) ])
+      phantom.stdout.on('data', function (o) { console.log(o.toString('utf8')) })
+      phantom.on('exit', function (e) {
+         callback(e?"/static/error.png":"/"+screenshot)
+         ++phantomProcess
+      })
+   })()
 }
 
 server.use('/static', express.static(__dirname+'/static/'))
